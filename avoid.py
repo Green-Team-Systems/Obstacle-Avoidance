4992b95d6abb34e7410a200cc1026ff8a5a4164c
Avi <amikoshdube@gmail.com>
2021-09-17 00:33:25 -0400

    Fixed Terrain math and movement

--- avoid.py
+++ avoid.py
@@ -8,6 +8,12 @@ import argparse
 import pprint
 import numpy
 
+# TODO: 
+# Create different responses based on LiDar data returned: yaw for trees,
+# vertical for walls, sloped for hills/ terrain
+# Clean up everything in the loop
+
+
 class LidarTest:
 
     def __init__(self):
@@ -16,6 +22,83 @@ class LidarTest:
         self.client = airsim.MultirotorClient()
         self.client.confirmConnection()
         self.client.enableApiControl(True)
+
+    def execute(self):
+        """
+        This is where your function description.
+        """
+        # Allows for user input of location and velocity variables from terminal for quicker testing
+        # xCord = input("Enter the X-Coordinate of destination:")
+        # yCord = input("Enter the Y-Coordinate of destination:")
+        # zCord = input("Enter the Z-Coordinate of destination:")
+        # droneVelocity = input("Enter the speed at which to travel to the destination:")
+
+        # Initialization of variables and drone
+        xCord = 400 # meters
+        yCord = 0 # meters
+        zCord = 0 # meters
+        droneVelocity = 10 # meters / second
+        count = 0
+        rotation = 0.0 # TODO Fifgure this out
+        collisionImminent = 3
+        xCordNew = (xCord * math.cos(rotation) + yCord * math.sin(rotation)) / (xCord + yCord)
+        yCordNew = (-xCord * math.sin(rotation) + yCord * math.cos(rotation)) / (xCord + yCord)
+        obstacleDetected = False
+        print("arming the drone...")
+        self.client.armDisarm(True)
+
+        # Takeoff of Drone
+        state = self.client.getMultirotorState()
+        s = pprint.pformat(state)
+        print(s)
+        airsim.wait_key('Press any key to takeoff')
+        self.client.takeoffAsync().join()
+        state = self.client.getMultirotorState()    
+        
+        # Waitkey to begin Lidar drone testing.
+        airsim.wait_key('Press any key to get Lidar readings')
+
+        # Move the drone to the coordinates initialized above
+        # MovetoPosition will use Airsim Path Planning while moveByVelocity will not, i.e. moveByVelocity will be less jumpy.
+        # self.client.moveByVelocityAsync(xCord, yCord, zCord, droneVelocity)
+        self.client.moveToPositionAsync(xCord, yCord, zCord, droneVelocity)
+
+        # Loop for Lidar testing
+        # The variable value of 400 was used to ensure a long enough time to thoroughly test
+        while 1 == 1:
+
+            # see getLidarData below.
+            lidarData = self.client.getLidarData()
+            ballsData = self.client.getDistanceSensorData(distance_sensor_name = "Balls")
+            cockData = self.client.getDistanceSensorData(distance_sensor_name = "Cock")
+
+            # prints the data collected from the distance sensors
+            print(f'Z-Distance sensor data: {ballsData.distance}')
+            print(f'X-Distance sensor data: {cockData.distance}')
+
+            slope = ballsData.distance / cockData.distance
+            hypo = math.sqrt(math.pow(ballsData.distance, 2) + math.pow(cockData.distance, 2))
+            zVelocity = ballsData.distance / hypo 
+            xVelocity = cockData.distance / hypo 
+            if cockData.distance < (0.5 * droneVelocity):
+                zVelocity = droneVelocity
+                xVelocity = 0
+            elif ballsData.distance < 10 or cockData.distance < (0.75 * droneVelocity):
+                zVelocity = zVelocity * droneVelocity
+                xVelocity = xVelocity * droneVelocity
+            elif ballsData.distance > 20:
+                zVelocity = (10 - ballsData.distance)
+                xVelocity = 0
+            else:
+                zVelocity = 0 
+                xVelocity = droneVelocity
+
+
+            print(f'Z speed: {zVelocity}')
+            print(f'X speed: {xVelocity}')
+            
+            self.client.moveByVelocityAsync(xVelocity, 0, -zVelocity, 0.01)
+            time.sleep(0.01)
 
     def parse_lidarData(self, data):
 
@@ -39,110 +122,10 @@ class LidarTest:
         self.client.enableApiControl(False)
         print("Done!\n")
 
-
-    def takeoff(self):
-
-        # Takeoff of Drone
-        state = self.client.getMultirotorState()
-        s = pprint.pformat(state)
-        print(s)
-        airsim.wait_key('Press any key to takeoff')
-        self.client.takeoffAsync().join()
-        state = self.client.getMultirotorState()   
-
-
-    def get_Lidar(self, collisionImminent, droneVelocity, obstacleDetected):
-        """
-        This function gets lidar data, decides if collision is imminent, and sets new trajectory
-        """
-
-
-        # Lidar testing
-        for i in range(1,400):
-            lidarData = self.client.getLidarData()
-
-            # Depending on the range of the Lidar sensor (in the settings.json) no points will be recieved if the points distance exceeds the range.
-            if (len(lidarData.point_cloud) < 3):
-                print("\tNo points received from Lidar data")
-
-
-            else:
-                # Divides the lidarData into 3 seperate variables for x, y and z
-                points = self.parse_lidarData(lidarData)
-                x_points, y_points, z_points = numpy.array(points[:, 0]),numpy.array(points[:, 1]), numpy.array(points[:,2])
-
-                # Returns timing data and other useful information for testing of Lidar
-                # print("\tReading %d: time_stamp: %d number_of_points: %d" % (i, lidarData.time_stamp, len(points)))
-                # print("\t\tlidar orientation: %s" % (pprint.pformat(lidarData.pose.orientation)))
-
-
-                # looping through returned points and checking if they are less than 'collisionImminent'.
-                # If they are less than this value, then 'obstacleDetected' is set to true.
-                for distance in z_points:
-
-                    if (distance < collisionImminent):
-                        self.client.hoverAsync()
-                        print('Obstacle Detected!')
-                        obstacleDetected = True
-
-                # Only entered if an Obstacle is detected by being less than 'collisionImminent'
-                if(obstacleDetected == True):
-                    print('Avoiding')
-
-                    # Calculations used to ascend a mountain by using the slope of the terrain.
-                    terrainSquared = abs(z_points[0] - (z_points[len(z_points) - 1]))
-                    terrain = math.sqrt(terrainSquared)
-
-                    # # These block is to activate the collision avoidance based on slope calculation.
-                    # # .join() is not used to keep functions from stepping on each other.
-                    self.client.moveToPositionAsync(
-                        lidarData.pose.position.x_val - terrain,
-                        lidarData.pose.position.y_val,
-                        (lidarData.pose.position.z_val - terrainSquared),
-                        droneVelocity
-                        )
-
-                    time.sleep(1)
-                    obstacleDetected = False
-
-
-    def execute(self):
-        # Initialization of variables and drone
-        xCord = 400 # meters
-        yCord = 0 # meters
-        zCord = 0 # meters
-        droneVelocity = 2 # meters / second
-        collisionImminent = 3
-        rotation = 0
-        xCordNew = (xCord * math.cos(rotation) + yCord * math.sin(rotation)) / (xCord + yCord)
-        yCordNew = (-xCord * math.sin(rotation) + yCord * math.cos(rotation)) / (xCord + yCord)
-        obstacleDetected = False
-        print("arming the drone...")
-        self.client.armDisarm(True)
-
-        # drone takeoff
-        LidarTest.takeoff(self)
-
-        # Waitkey to begin Lidar drone testing.
-        airsim.wait_key('Press any key to get Lidar readings')
-
-        self.client.moveToPositionAsync(xCord, yCord, zCord, droneVelocity)
-
-        # Get lidar data and calculate for obstacle avoidance 
-        LidarTest.get_Lidar(self, collisionImminent, droneVelocity, obstacleDetected)
-
-        #self.client.moveToPositionAsync(xCord, yCord, zCord, droneVelocity, 1)
-        time.sleep(1)
-
-
-
-
 # main
 if __name__ == "__main__":
     args = sys.argv
     args.pop(0)
-
-
     arg_parser = argparse.ArgumentParser("Lidar.py makes drone fly and gets Lidar data")
 
     arg_parser.add_argument('-save-to-disk', type=bool, help="save Lidar data to disk", default=False)
