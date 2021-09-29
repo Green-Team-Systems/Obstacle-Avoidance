f2e43ea4350c2e9e5b32b52b5ac000a60bf91fe6
Josh Chang <chang529@purdue.edu>
2021-09-29 14:28:59 -0400

    pointcloud script

---
+++ lidar_pointcloud.py
@@ -0,0 +1,125 @@
+# Python client example to get Lidar data from a drone
+#
+
+import setup_path 
+import airsim
+
+import sys
+import math
+import time
+import argparse
+import pprint
+import numpy as np
+# import open3d as o3d
+
+# Makes the drone fly and get Lidar data
+class LidarTest:
+
+    def __init__(self):
+
+        # connect to the AirSim simulator
+        self.client = airsim.MultirotorClient()
+        self.client.confirmConnection()
+        self.client.enableApiControl(True)
+
+    def takeoff(self):
+
+        # Takeoff of Drone
+        state = self.client.getMultirotorState()
+        s = pprint.pformat(state)
+        print(s)
+        airsim.wait_key('Press any key to takeoff')
+        self.client.takeoffAsync().join()
+        state = self.client.getMultirotorState()   
+
+    def execute(self,vehicle_name,lidar_names):
+
+        print("arming the drone...")
+        self.client.armDisarm(True)
+
+        self.takeoff()
+
+        airsim.wait_key('Press any key to move vehicle to (-10, 10, -10) at 5 m/s')
+        self.client.moveToPositionAsync(0, 0, -5, 5).join()
+
+        self.client.hoverAsync().join()
+
+        print('Scanning Has Started\n')
+        print('Use Keyboard Interrupt \'CTRL + C\' to Stop Scanning\n')
+        existing_data_cleared = False   #change to true to superimpose new scans onto existing .asc files
+        try:
+            while True:
+                for lidar_name in lidar_names:
+                    filename = f"{vehicle_name}_{lidar_name}_pointcloud.asc"
+                    if not existing_data_cleared:
+                        f = open(filename,'w')
+                    else:
+                        f = open(filename,'a')
+                    lidar_data = self.client.getLidarData(lidar_name=lidar_name,vehicle_name=vehicle_name)
+                    print(lidar_data)
+                    orientation = lidar_data.pose.orientation
+                    q0, q1, q2, q3 = orientation.w_val, orientation.x_val, orientation.y_val, orientation.z_val
+                    rotation_matrix = np.array(([1-2*(q2*q2+q3*q3),2*(q1*q2-q3*q0),2*(q1*q3+q2*q0)],
+                                                  [2*(q1*q2+q3*q0),1-2*(q1*q1+q3*q3),2*(q2*q3-q1*q0)],
+                                                  [2*(q1*q3-q2*q0),2*(q2*q3+q1*q0),1-2*(q1*q1+q2*q2)]))
+
+                    position = lidar_data.pose.position
+                    for i in range(0, len(lidar_data.point_cloud), 3):
+                        xyz = lidar_data.point_cloud[i:i+3]
+
+                        corrected_x, corrected_y, corrected_z = np.matmul(rotation_matrix, np.asarray(xyz))
+                        final_x = corrected_x + position.x_val
+                        final_y = corrected_y + position.y_val
+                        final_z = corrected_z + position.z_val
+
+                        f.write("%f %f %f %d %d %d \n" % (final_x,final_y,final_z,255,255,0))
+                    f.close()
+                existing_data_cleared = True
+        except KeyboardInterrupt:
+            airsim.wait_key('Press any key to stop running this script')
+            print("Done!\n")
+
+        
+
+    def parse_lidarData(self, data):
+
+        # reshape array of floats to array of [X,Y,Z]
+        points = np.array(data.point_cloud, dtype=np.dtype('f4'))
+        points = np.reshape(points, (int(points.shape[0]/3), 3))
+       
+        return points
+
+    def write_lidarData_to_disk(self, points):
+        # TODO
+        print("not yet implemented")
+
+    def stop(self):
+
+        airsim.wait_key('Press any key to reset to original state')
+
+        self.client.armDisarm(False)
+        self.client.reset()
+
+        self.client.enableApiControl(False)
+        print("Done!\n")
+
+
+    # def display_lidar(self):
+    #     pcd = o3d.io.read_point_cloud("../Drone1_LidarSensor1_pointcloud.asc")
+    #     print(pcd)
+# main
+if __name__ == "__main__":
+    args = sys.argv
+    args.pop(0)
+
+    arg_parser = argparse.ArgumentParser("Lidar.py makes drone fly and gets Lidar data")
+
+    arg_parser.add_argument('-save-to-disk', type=bool, help="save Lidar data to disk", default=False)
+  
+    args = arg_parser.parse_args(args)    
+    lidarTest = LidarTest()
+    try:
+        lidarTest.execute('Drone1',['LidarSensor1'])
+    finally:
+        # lidarTest.display_lidar()
+        lidarTest.stop()
